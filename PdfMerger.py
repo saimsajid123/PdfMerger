419afd7431bc62ef5bc0e16b7529522f30c1d20d
Muhammad Saim Sajid <141317850+saimsajid123@users.noreply.github.com>
2024-01-15 23:55:49 +0500

    Update PdfMerger.py

--- PdfMerger.py
+++ PdfMerger.py
@@ -1,14 +1,51 @@
-
+import tkinter as tk
+from tkinter import filedialog
 from PyPDF2 import PdfMerger
 
-pdfs=["CamScanner 10-29-2021 14.58.pdf","Result Of All Semesters Combined.pdf"]
+class PDFMergerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("PDF Merger")
 
-#first double qoute should contain the name of the pdfs you wanna merge and the 2nd double qoute aka the last one should contain the name of the pdf that will be created as one merged pf
+        self.file_list = []
 
-merger=PdfMerger()
+        self.create_widgets()
 
-for i in pdfs:
-    merger.append(i)
+    def create_widgets(self):
+        self.listbox = tk.Listbox(self.root, selectmode=tk.MULTIPLE)
+        self.listbox.pack(pady=10)
 
-merger.write("Test.pdf")
-merger.close()
+        self.add_button = tk.Button(self.root, text="Add PDFs", command=self.add_pdfs)
+        self.add_button.pack(pady=5)
+
+        self.merge_button = tk.Button(self.root, text="Merge PDFs", command=self.merge_pdfs)
+        self.merge_button.pack(pady=5)
+
+    def add_pdfs(self):
+        files = filedialog.askopenfilenames(title="Select PDF files", filetypes=[("PDF files", "*.pdf")])
+        if files:
+            for file in files:
+                self.listbox.insert(tk.END, file)
+            self.file_list.extend(files)
+
+    def merge_pdfs(self):
+        if not self.file_list:
+            tk.messagebox.showwarning("Warning", "No PDFs selected.")
+            return
+
+        merger = PdfMerger()
+        for pdf_file in self.file_list:
+            merger.append(pdf_file)
+
+        output_file = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
+        if output_file:
+            merger.write(output_file)
+            tk.messagebox.showinfo("Success", "PDFs merged successfully.")
+
+            self.file_list = []
+            self.listbox.delete(0, tk.END)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PDFMergerApp(root)
+    root.mainloop()
